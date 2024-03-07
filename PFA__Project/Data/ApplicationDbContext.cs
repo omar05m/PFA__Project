b3820799d45bfeadfd99a4f607e58c806b991324
Khalil Mohammed Berkani <khalilikafo@gmail.com>
2024-03-07 21:02:54 +0100

    ajoute de la configuration de la base de donnee

---
+++ PFA__Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PFA_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {          
+        }
+
+
+
+    }
+}
