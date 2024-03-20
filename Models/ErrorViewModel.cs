98d2bf5dec09a4a70459d89ca33d9fdb8180f46c
OmarBou <124223569+omar05m@users.noreply.github.com>
2024-03-20 23:52:43 +0100

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PFA__Project.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
